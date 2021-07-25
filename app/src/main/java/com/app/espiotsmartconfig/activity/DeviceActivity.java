84573707ac87d40bc5df4286371e4f885b2d4020
hutaoCode <643759269@qq.com>
2021-07-25 20:09:26 +0800

    1、智能打火机接入

--- app/src/main/java/com/app/espiotsmartconfig/activity/DeviceActivity.java
+++ app/src/main/java/com/app/espiotsmartconfig/activity/DeviceActivity.java
@@ -8,6 +8,7 @@ import com.app.espiotsmartconfig.base.BaseActivity;
 import com.app.espiotsmartconfig.cache.Const;
 import com.app.espiotsmartconfig.core.BspHelper;
 import com.app.espiotsmartconfig.model.Device;
+import com.app.espiotsmartconfig.widget.CustomFireView;
 import com.app.espiotsmartconfig.widget.HeaderView;
 
 public class DeviceActivity extends BaseActivity {
@@ -25,23 +26,42 @@ public class DeviceActivity extends BaseActivity {
                 .setBackVisible(true)
                 .setMenuVisible(false);
 
-        ImageView imageView = findViewById(R.id.ivFire);
-        imageView.setOnClickListener(view -> {
-            if (isOn) return;
-            isOn = true;
-            BspHelper.pushData("1");
-            imageView.setImageResource(isOn ? R.mipmap.on_fire : R.mipmap.off_fire);
-            if (isOn) {
-                //3秒之后，自动关闭打火机
-                mHandler.postDelayed(() -> {
-                    if (isOn) {
-                        BspHelper.pushData("0");
-                        isOn = false;
-                        imageView.setImageResource(isOn ? R.mipmap.on_fire : R.mipmap.off_fire);
-                    }
-                }, 5000);
+        CustomFireView imageView = findViewById(R.id.ivFire);
+//        CustomFireView.setOnClickListener(view -> {
+//            if (isOn) return;
+//            isOn = true;
+//            BspHelper.pushData("1");
+//            imageView.setImageResource(isOn ? R.mipmap.on_fire : R.mipmap.off_fire);
+//            if (isOn) {
+//                //3秒之后，自动关闭打火机
+//                mHandler.postDelayed(() -> {
+//                    if (isOn) {
+//                        BspHelper.pushData("0");
+//                        isOn = false;
+//                        imageView.setImageResource(isOn ? R.mipmap.on_fire : R.mipmap.off_fire);
+//                    }
+//                }, 5000);
+//            }
+//        });
+
+        imageView.setCallbackListener(new CustomFireView.CallbackListener() {
+            @Override
+            public void openFire() {
+                if (!isOn) {
+                    isOn = true;
+                    imageView.setImageResource(isOn ? R.mipmap.on_fire : R.mipmap.off_fire);
+                    BspHelper.pushData("1");
+                }
+            }
+
+            @Override
+            public void closeFire() {
+                if (isOn) {
+                    isOn = false;
+                    imageView.setImageResource(isOn ? R.mipmap.on_fire : R.mipmap.off_fire);
+                    BspHelper.pushData("0");
+                }
             }
         });
-
     }
 }
---
+++ app/src/main/java/com/app/espiotsmartconfig/widget/CustomFireView.java
@@ -0,0 +1,68 @@
+package com.app.espiotsmartconfig.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+@SuppressLint("AppCompatCustomView")
+public class CustomFireView extends ImageView {
+
+    private static final String TAG = "CustomFireView";
+
+    public CustomFireView(Context context) {
+        super(context);
+    }
+
+    public CustomFireView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomFireView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private float downY;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float moveY = event.getY();
+                if (moveY - downY > 200) {
+                    if (callbackListener != null) {
+                        Log.i(TAG, "callbackListener.openFire() 开始执行...");
+                        callbackListener.openFire();
+                        downY = event.getY();
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                if (callbackListener != null) {
+                    callbackListener.closeFire();
+                    Log.i(TAG, "callbackListener.closeFire() 开始执行...");
+                    downY = event.getY();
+                }
+                break;
+        }
+        return true;
+    }
+
+    public interface CallbackListener{
+        void openFire();
+        void closeFire();
+    }
+
+    private CallbackListener callbackListener;
+
+    public void setCallbackListener(CallbackListener callbackListener) {
+        this.callbackListener = callbackListener;
+    }
+}
